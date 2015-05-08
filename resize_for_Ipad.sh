d5c34b90394a5b68cd1462cfb3725b751518f039
Gennady Borukhovich <gennady@intuary.com>
2015-05-08 15:35:19 -0400

    updated to latest universal

--- resize_for_Ipad.sh
+++ resize_for_Ipad.sh
@@ -2,11 +2,18 @@
 # Script to resize an app icon for all necessary sizes for iOS apps
 # Assumptions:
 
-convert Icon-512.png -resize 72x72 Icon-72.png
-convert Icon-512.png -resize 144x144 Icon-72@2x.png
-convert Icon-512.png -resize 50x50 Icon-Small-50.png
-convert Icon-512.png -resize 100x100 Icon-Small-50@2x.png
-convert Icon-512.png -resize 29x29 Icon-Small.png
-convert Icon-512.png -resize 58x58 Icon-Small@2x.png
-convert Icon-512.png -resize 512x512 iTunesArtwork
+convert $1 -resize 512x512	${2}/iTunesArtwork
+convert $1 -resize 1024x1024	${2}/iTunesArtwork@2x
+convert $1 -resize 120x120	${2}/Icon-60@2x.png
+convert $1 -resize 180x180	${2}/Icon-60@3x.png
+convert $1 -resize 76x76	${2}/Icon-76.png
+convert $1 -resize 152x152	${2}/Icon-76@2x.png
+convert $1 -resize 40x40	${2}/Icon-Small-40.png
+convert $1 -resize 80x80	${2}/Icon-Small-40@2x.png
+convert $1 -resize 120x120	${2}/Icon-Small-40@3x.png
+convert $1 -resize 29x29	${2}/Icon-Small.png
+convert $1 -resize 58x58	${2}/Icon-Small@2x.png
+convert $1 -resize 87x87	${2}/Icon-Small@2x.png
+
+
 
